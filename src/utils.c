d0ddc50a29aee1c120d3f4f49cc6520aad018839
owalsh <owalsh@student.42.fr>
2022-08-01 16:28:11 +0200

    Modify display simulation

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:26:39 by owalsh            #+#    #+#             */
-/*   Updated: 2022/08/01 16:26:52 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/08/01 16:27:57 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ void	*ft_memset(void *s, int c, size_t n)
 
 void	display_simulation(t_sim *data)
 {
-	printf("philo->number = %d\n", data->number);
-	printf("philo->time_to_die = %d\n", data->time_to_die);
-	printf("philo->time_to_eat = %d\n", data->time_to_eat);
-	printf("philo->time_to_sleep = %d\n", data->time_to_sleep);
-	printf("philo->meals_per_philo = %d\n", data->meals_per_philo);
+	printf("sim->number = %d\n", data->number);
+	printf("sim->time_to_die = %d\n", data->time_to_die);
+	printf("sim->time_to_eat = %d\n", data->time_to_eat);
+	printf("sim->time_to_sleep = %d\n", data->time_to_sleep);
+	printf("sim->meals_per_philo = %d\n", data->meals_per_philo);
 }
